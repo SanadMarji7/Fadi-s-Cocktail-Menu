c2ee3ffaf677d4b4b465ed38bc181416495b5442
Sanad Marji <74622693+SanadMarji7@users.noreply.github.com>
2023-12-23 13:32:57 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add interactivity here - this is just a placeholder example
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
-    sections.forEach(section => {
-        section.addEventListener('click', function () {
-            this.classList.toggle('highlight');
-        });
-    });
-});
